7371c160f784056a23353091c0ed315655e1d5f5
AliSultonsPalilati <105841102222@student.unismuh.ac.id>
2025-08-17 16:08:27 +0800

    Koreksi

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,13 @@ import React from 'react';
 export default function RootLayout() {
   return (
     <Stack>
+      {/* Mendaftarkan grup auth */}
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      
+      {/* Mendaftarkan grup tabs */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       
+      {/* DIUBAH: Mendaftarkan grup product, bukan file di dalamnya */}
       <Stack.Screen name="(product)" options={{ headerShown: false }} />
     </Stack>
   );
